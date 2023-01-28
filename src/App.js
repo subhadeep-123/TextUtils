fba8d4fe40220e4176c91ff6298d94477ccb4bc8
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-28 18:28:53 +0530

    Add about us page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import TextForm from "./components/TextForm";
-
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" about="About TextUtils" />
-      <div className="container my-3">
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      {/* <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
-      </div>
+      </div> */}
+      <About />
     </>
   );
 }
