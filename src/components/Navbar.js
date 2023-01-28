be3914ca11538290b36f33e9c4446dd72847c0c3
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-28 16:59:50 +0530

    Make navbar dark

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ Navbar.defaultProps = {
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
