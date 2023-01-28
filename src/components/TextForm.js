cd2d9433844c57a6ac76dab48c28d4c14d55dea3
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-28 17:00:32 +0530

    Add lowercase and clear option with real-time work count and preview logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
 
   const handleUpClick = () => {
     setText(text.toUpperCase());
   };
 
+  const handleLowClick = () => {
+    setText(text.toLowerCase());
+  };
+
   const clearBox = () => {
     setText("");
   };
-  
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -19,39 +23,34 @@ export default function TextForm(props) {
     <>
       <div className="container">
         <h1>{props.heading}</h1>
-        <form>
-          <textarea
-            className="form-control"
-            value={text}
-            id="myBox"
-            onChange={handleOnChange}
-            rows="8"
-          ></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          id="myBox"
+          onChange={handleOnChange}
+          rows="8"
+        ></textarea>
 
-          <div className="my-3">
-            <button
-              type="submit"
-              className="btn btn-success"
-              onClick={handleUpClick}
-            >
-              Convert to uppercase
-            </button>
-            <button
-              type="reset"
-              className="btn btn-success float-end"
-              onClick={clearBox}
-            >
-              Clear
-            </button>
-          </div>
-        </form>
+        <div className="my-3">
+          <button className="btn btn-success" onClick={handleUpClick}>
+            Uppercase
+          </button>
+          <button className="btn btn-success mx-1" onClick={handleLowClick}>
+            Lower
+          </button>
+          <button className="btn btn-success float-end" onClick={clearBox}>
+            Clear
+          </button>
+        </div>
       </div>
       <div className="container my-5">
         <h3>Your text summary</h3>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
+        <p> {0.008 * text.split(" ").length} Minutes read</p>
       </div>
+      <div className="container">{text}</div>
     </>
   );
 }
