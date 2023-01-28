066e5b1876b1cb694e5d061d1e696ec95814cafa
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-28 19:57:37 +0530

    Using react route to route pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import TextForm from "./components/TextForm";
+import NoPage from "./components/NoPage";
+
 function App() {
   return (
-    <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
-      {/* <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
-      </div> */}
-      <About />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navbar title="TextUtils" aboutText="About TextUtils" />}
+        />
+        <Route
+          index
+          element={<TextForm heading="Enter the text to analyze below" />}
+        />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
