d7f11fa6a7f998bdad9dc1ba5f9530803e63dcfe
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-28 21:03:13 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ import NoPage from "./components/NoPage";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navbar title="TextUtils" aboutText="About TextUtils" />}
-        />
-        <Route
-          index
-          element={<TextForm heading="Enter the text to analyze below" />}
-        />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
+      <div>
+        <Navbar title="TextUtils" aboutText="About TextUtils" />
+        <Routes>
+          <Route
+            index
+            element={<TextForm heading="Enter the text to analyze below" />}
+          />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
