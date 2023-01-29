75a995c05f61f2f8d47c1d4290899a191e53cb73
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-01-30 00:24:19 +0530

    Fix page reload issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import Alert from "./Alert";
 
 Navbar.propTypes = {
@@ -20,9 +21,9 @@ export default function Navbar(props) {
           className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
         >
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               {props.title}
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -40,14 +41,14 @@ export default function Navbar(props) {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <Link className="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
+                  <Link className="nav-link" to="/about">
                     {props.aboutText}
-                  </a>
+                  </Link>
                 </li>
               </ul>
               <div
